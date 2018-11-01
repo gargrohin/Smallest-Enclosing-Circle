5e998ce5fb35f2dab23fbe7c16f14813a5ccda5e
Rohin Garg <gargrohin0@gmail.com>
2018-11-01 20:52:40 +0530

    Compile errors fixed

--- test.c
+++ test.c
@@ -1,29 +1,30 @@
 #include <LEDA/core/list.h>
 #include <LEDA/geo/point.h>
 #include <LEDA/geo/circle.h>
-#include <vector>
 
 using namespace leda;
 using namespace std;
 
-circle f1(){
+circle f3(list<point> l, point p, point q){
   int n = l.length();
   int i=0;
-  list<point> l2;
+  point r;
+  point cent = midpoint(p,q);
+  circle C = circle(cent,p);
   for(i=0;i<n;i++){
-    point p = l.pop();
-    if(!(C.inside(p)||C.contains(p))){
-      C = circle(p);
-      C = f2(l2,p);
+    r=l.pop();
+    if(!(C.inside(r)||C.contains(r))){
+      C = circle(p,q,r);
     }
-    l2.push(p);
   }
   return C;
 }
 
-circle f2(){
+circle f2(list<point> l, point p){
   int n = l.length();
   int i=0;
+  point q;
+  circle C;
   list<point> l2;
   for(i=0;i<n;i++){
     q=l.pop();
@@ -39,35 +40,24 @@ circle f2(){
   return C;
 }
 
-circle f3(){
+circle f1(list<point> l, circle C){
   int n = l.length();
   int i=0;
-  point cent = midpoint(p,q);
-  C = circle(cent,p);
+  list<point> l2;
   for(i=0;i<n;i++){
-    r=l.pop();
-    if(!(C.inside(r)||C.contains(r))){
-      C = circle(p,q,r);
+    point p = l.pop();
+    if(!(C.inside(p)||C.contains(p))){
+      C = circle(p);
+      C = f2(l2,p);
     }
+    l2.push(p);
   }
   return C;
 }
 
+
 int main()
 {
-  point a(2,4);
-  point b(3,5);
-  point c(1,6);
-  circle C(a,b,c);
-  if(C.inside(a)|| C.contains(a)){
-    std:: cout << 1<<std::endl;
-  }
-  list<point> l;
-  l.push(a);
-  l.push(b);
-  l.push(c);
-  std::cout<<l<<" + "<<l.pop()<<" + " << l;
-  C= circle(a);
-  cout<<C.center();
+  int i=0;
   return 0;
 }
