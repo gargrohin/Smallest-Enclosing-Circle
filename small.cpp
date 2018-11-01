cb828d3a145b6f34b0ca3b2f5d46daa9c5e02e8e
Sudhanshu Jaiswal <sudsjaiswal@gmail.com>
2018-11-01 23:21:56 +0530

    Made a c++ file for easier work

---
+++ small.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <LEDA/core/list.h>
+#include <LEDA/geo/point.h>
+#include <LEDA/geo/circle.h>
+
+using namespace leda;
+using namespace std;
+
+circle f3(list<point> l, point p, point q){
+  int n = l.length();
+  int i=0;
+  point r;
+  point cent = midpoint(p,q);
+  circle C = circle(cent,p);
+  for(i=0;i<n;i++){
+    r=l.pop();
+    if(!(C.inside(r)||C.contains(r))){
+      C = circle(p,q,r);
+    }
+  }
+  return C;
+}
+
+circle f2(list<point> l, point p){
+  int n = l.length();
+  int i=0;
+  point q;
+  circle C;
+  list<point> l2;
+  for(i=0;i<n;i++){
+    q=l.pop();
+    if(i==0){
+      point cent = midpoint(p,q);
+      C=circle(cent,p);
+    }
+    if(!(C.inside(q)||C.contains(q))){
+      C = f3(l2,p,q);
+    }
+    l2.push(q);
+  }
+  return C;
+}
+
+circle f1(list<point> l, circle C){
+  int n = l.length();
+  int i=0;
+  list<point> l2;
+  for(i=0;i<n;i++){
+    point p = l.pop();
+    if(!(C.inside(p)||C.contains(p))){
+      C = circle(p);
+      C = f2(l2,p);
+    }
+    l2.push(p);
+  }
+  return C;
+}
+
+
+int main()
+{
+  int i=0;
+  cout << "this is working in c++\n";
+  return 0;
+}
